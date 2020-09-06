6e13b9fdbad8fb873f3072a9e8f5b89c97b65154
SayanthD <sayanthd@gmail.com>
2020-09-07 00:47:41 +0530

    youtube_dl_button: return fulltitle, not description

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -86,7 +86,7 @@ async def youtube_dl_call_back(bot, update):
     )
     description = "@PublicLeech"
     if "fulltitle" in response_json:
-        description = response_json["fulltitle"][0:1021]
+        fulltitle = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
     if "description" in response_json:
         description = response_json["description"][0:1021]
@@ -179,7 +179,7 @@ async def youtube_dl_call_back(bot, update):
             user_id,
             {},
             True,
-            description
+            fulltitle
         )
         LOGGER.info(final_response)
         #
